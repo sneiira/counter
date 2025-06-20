a96a23b16f33d11a1f09c5de44084a885995a639
s.neira <151532950+sneiira@users.noreply.github.com>
2025-06-20 17:09:41 +0200

    animation buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(null)
   const [s, setS] = useState(null)
+  const [animate, setAnimate] = useState(false)
+
+  useEffect(() => {
+  // Activa la animación al cambiar count
+    if (count !== null) {
+      setAnimate(true)
+      const timeout = setTimeout(() => setAnimate(false), 500)
+      return () => clearTimeout(timeout)
+    }
+  }, [count])
 
   return (
     <>
-     <h1>Contador</h1>
-     <input className='contador' type='number' value={count} onChange={(e) => setCount(Number(e.target.value))}  style={{ width: `${String(count).length + 1}ch` }}/>
-     <div className='boton'>
-      <button  onClick={() => setCount((count) => count + s)} >+</button>
-      <button  onClick={() => setCount((count) => 0)} >reiniciar contador</button>
+     <h1>Counter</h1>
+     <input className={`counter ${animate ? 'animate' : ''}`} type='number' value={count} onChange={(e) => setCount(e.target.value === "" ? null : Number(e.target.value))}  style={{ width: `${String(count).length + 1}ch` }}/>
+     <div className='generic-buttons'>
+      <button className='button1' onClick={() => setCount((count) => count + s)} >+</button>
+      <button  className = 'button2' onClick={() => {setCount((count) => 0); setS((s) => 0)}} >reset</button>
      </div> 
-     <div className='cantidades'>
-      <label>añadir<br />
-       <input className= 'añadir'type="number" value={s} onChange={(e) => setS(Number(e.target.value))} style={{ width: `${String(s).length + 1}ch` }} />
+     <div className='cuantities'>
+      <label>agregate<br />
+       <input className= 'agregate'type="number" value={s} onChange={(e) => setS(e.target.value === "" ? null : Number(e.target.value))} style={{ width: `${String(s).length + 1}ch` }} />
       </label>
      </div>
     </>
