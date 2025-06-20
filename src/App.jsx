5330892f75ffb2e14778ae5e13087adac21f5f9e
s.neira <151532950+sneiira@users.noreply.github.com>
2025-06-20 15:29:17 +0200

    slight changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(null)
   const [s, setS] = useState(null)
-  const [r, setR] = useState(null)
 
   return (
     <>
@@ -12,16 +11,12 @@ function App() {
      <input className='contador' type='number' value={count} onChange={(e) => setCount(Number(e.target.value))}  style={{ width: `${String(count).length + 1}ch` }}/>
      <div className='boton'>
       <button  onClick={() => setCount((count) => count + s)} >+</button>
-      <button  onClick={() => setCount((count) => count - r)} >-</button>
       <button  onClick={() => setCount((count) => 0)} >reiniciar contador</button>
      </div> 
      <div className='cantidades'>
-      <label>Sumar:<br />
-       <input className= 's-r'type="number" value={s} onChange={(e) => setS(Number(e.target.value))} style={{ width: `${String(s).length + 1}ch` }} />
+      <label>añadir<br />
+       <input className= 'añadir'type="number" value={s} onChange={(e) => setS(Number(e.target.value))} style={{ width: `${String(s).length + 1}ch` }} />
       </label>
-        <label>Restar: <br />
-       <input className='s-r' type="number" value={r} onChange={(e) => setR(Number(e.target.value))} style={{ width: `${String(count).length + 1}ch` }} />
-       </label>
      </div>
     </>
   )
