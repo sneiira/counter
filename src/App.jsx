829eac79192dbcc2fbae56074532f200226f633b
s.neira <151532950+sneiira@users.noreply.github.com>
2025-06-25 19:14:37 +0200

    Better variable names

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [s, setS] = useState(1)
+  const [sum, setSum] = useState(1)
 
 
 
@@ -17,13 +17,13 @@ function App() {
         {count}
       </span>
      <div className='generic-buttons'>
-      <button className='button1' onClick={() => setCount((count) => count + s)} >+</button>
-      <button  className = 'button2' onClick={() => {setCount((count) => 0); setS((s) => 1)}} >reset</button>
+      <button className='button1' onClick={() => setCount((count) => count + sum)} >+</button>
+      <button  className = 'button2' onClick={() => {setCount(0); setSum(1)}} >reset</button>
      </div> 
      <div className='cuantities'>
       <label>agregate<br />
-       <input className= 'agregate'type="number" value={s} onChange={(e) => setS(e.target.value === "" ? null : Number(e.target.value))}
-        style={{ width: `${String(s).length + 1}ch`, maxWidth: '200px' }} />
+       <input className= 'agregate'type="number" value={sum} onChange={(e) => setSum(e.target.value === "" ? null : Number(e.target.value))}
+        style={{ width: `${String(sum).length + 1}ch` }} />
       </label>
      </div>
     </>
